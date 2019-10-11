4762f456d01f8fc6ab5083bed12d33e1f2899568
zhaoqi <303173868@qq.com>
2019-10-11 16:23:50 +0800

    Initial commit

---
+++ routes/yuyuyui.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('yuyuyui/index', { title: '闪光的花结' });
+});
+
+module.exports = router;
