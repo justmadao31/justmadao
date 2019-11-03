e85a463d776ebabbbc1ef94f0b7941c75761798a
justmadao31 <303173868@qq.com>
2019-11-03 22:49:47 +0800

    test

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('yuyuyui/index', {title: '闪光的花结', userInfo: req.session.userInfo});
-});
-
-module.exports = router;
