093ce9a8660c60a8d5a6efe95f93633f11a6fda0
justmadao31 <303173868@qq.com>
2019-11-03 22:35:07 +0800

    test

--- util/mysqlConnect.js
+++ util/mysqlConnect.js
@@ -2,11 +2,11 @@ const mysql = require('mysql')
 
 const connectdb=()=>{
     let connection = mysql.createConnection({
-        host     : '47.240.87.248',
+        host     : 'loaclhost',
         port     : '3306',
-        user     : 'zhaoqi',
-        password : '123456',
-        database : 'yuyuyui'
+        user     : '1111',
+        password : '2222',
+        database : '3333'
     })
     return connection;
 }
